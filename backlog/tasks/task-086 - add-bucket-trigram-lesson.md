27fb615acb4c99cf1dcdde4086017129fa7efbd1
Ben Swift <ben@benswift.me>
2025-12-12 08:50:54 +1100

    Update npm packages: vite-plugin-checker 0.12, vitepress-plugin-rss 0.4

---
+++ backlog/tasks/task-086 - add-bucket-trigram-lesson.md
@@ -0,0 +1,9 @@
+---
+id: task-086
+title: add bucket-trigram lesson
+status: To Do
+assignee: []
+created_date: "2025-12-11 21:50"
+labels: []
+dependencies: []
+---
--- website/.vitepress/theme/templates/index.ts
+++ website/.vitepress/theme/templates/index.ts
@@ -1,5 +1,5 @@
 // Import CLI templates as raw text using Vite's ?raw import
-import bookTemplate from '../../../../cli/book.typ?raw';
-import cutoutsTemplate from '../../../../cli/tokenized-cutouts.typ?raw';
+import bookTemplate from "../../../../cli/book.typ?raw";
+import cutoutsTemplate from "../../../../cli/tokenized-cutouts.typ?raw";
 
 export { bookTemplate, cutoutsTemplate };
